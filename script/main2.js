e1f0b1c840f17f685bb2c3b59a0c24454cc9599a
andrei_labadaev <53191183+ivolat05@users.noreply.github.com>
2021-07-22 23:12:13 +0300

    Add files via upload

--- script/main2.js
+++ script/main2.js
@@ -67,4 +67,33 @@ $(document).ready(function () {
 
     $('.authorization-popup-active').magnificPopup({});
 
+
+
+
+    // nouislider
+
+    const slaider = document.getElementById('slaider-line');
+
+    noUiSlider.create(slaider, {
+
+        start: [0, 5000],
+        connect: true,
+        range: {
+            'min': 0,
+            'max': 5000
+        }
+    });
+
+    let priceMin = document.getElementById('priceMin'),
+        priceMax = document.getElementById('priceMax');
+
+
+    slaider.noUiSlider.on('update', function (values, handle) {
+        if (handle) {
+            priceMax.innerHTML = values[handle];
+        } else {
+            priceMin.innerHTML = values[handle];
+        }
+    });
+
 });
